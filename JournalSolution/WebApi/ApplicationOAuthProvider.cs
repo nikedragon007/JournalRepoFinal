e663d712f48d65e431a054f8cb1c1d141185e092
Nikesh A <nikesh1408@hotmail.com>
2018-06-17 01:48:06 +0530

    OAuth implementation

---
+++ JournalSolution/WebApi/ApplicationOAuthProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using WebApi.Models;
+
+namespace WebApi
+{
+    public class ApplicationOAuthProvider:OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
+            var manager = new UserManager<ApplicationUser>(userStore);
+            var user = await manager.FindAsync(context.UserName,context.Password);
+            if (user != null)
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim("Username",user.UserName));
+                identity.AddClaim(new Claim("Email", user.Email));
+                context.Validated(identity);
+            }
+            else return;
+        }
+
+    }
+}
---
+++ JournalSolution/WebApi/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebApi.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebApi.Controllers
+{
+    public class AccountController : ApiController
+    {
+        // GET: Account
+        [Route("api/User/Register")]
+        [HttpPost]
+        public IdentityResult Register(AccountModel model)
+        {
+            try
+            {
+                model.UserName = "nik";
+                model.Email = "nikedragon007@gmail.com";
+                model.Password = "nik";
+                var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
+                var manager = new UserManager<ApplicationUser>(userStore);
+                var user = new ApplicationUser() { UserName = model.UserName, Email = model.Email };
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                manager.PasswordValidator = new PasswordValidator
+                {
+                    RequiredLength = 3
+                };
+                IdentityResult result = manager.Create(user, model.Password);
+                return result;
+            }
+            catch(Exception ex)
+            {
+                return new IdentityResult();
+            }
+
+        }
+    }
+}
